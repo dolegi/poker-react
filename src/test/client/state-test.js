ebbbf4d3dce0a954b7c045970fb9c2ec7980332b
Dominic Ginger <domginger@hotmail.co.uk>
2017-01-25 08:09:52 +0000

    Example deck test

---
+++ src/test/client/state-test.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies, no-unused-expressions */
+  /*
+import assert from 'assert';
+import { createStore } from 'redux';
+import { combineReducers } from 'redux-immutable';
+import { describe, it, beforeEach } from 'mocha';
+import deckReducer from '../../client/reducers/deck-reducer';
+import { shuffleDeck } from '../../client/actions/deck-actions';
+
+let store;
+
+describe('App State', () => {
+  describe('Deck', () => {
+    beforeEach(() => {
+      store = createStore(combineReducers({
+        deck: deckReducer,
+      }));
+    });
+  });
+});
+*/
---
+++ src/test/shared/deck-test.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+//
+//import assert from 'assert';
+//import { stub } from 'sinon';
+//import { describe, it } from 'mocha';
+//import Deck from '../../shared/deck';
+//
+//describe('Shared', () => {
+//  describe('Deck', () => {
+//    describe('print', () => {
+//      it('prints the cards array', () => {
+//        stub(console, 'log');
+//        new Deck().print();
+//        assert(console.log.calledOnce);
+//        console.log.restore();
+//      });
+//    });
+//  });
+//});
