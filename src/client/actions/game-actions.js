5928ccf43ea84074be2e347c9623c4320df76070
Dominic Ginger <domginger@hotmail.co.uk>
2017-01-31 23:27:39 +0000

    Create initial state and begin action

--- src/client/actions/game-actions.js
+++ src/client/actions/game-actions.js
@@ -1,10 +1,7 @@
 import { createAction } from 'redux-actions';
-import Game from '../../shared/game';
 
 export const BEGIN = 'BEGIN';
 export const begin = createAction(BEGIN, () => {
-//  const game = new Game([player].concat(opponents));
-//  game.dealPlayers();
-  return { game: x => new Game(x) };
+  return { game: x => x };
 });
 
--- src/client/reducers/game-reducer.js
+++ src/client/reducers/game-reducer.js
@@ -1,22 +1,51 @@
 import { BEGIN } from '../actions/game-actions';
-import Player from '../../shared/player';
+import Deck from '../../shared/deck';
+
+const createPlayer = name => ({
+  name,
+  chips: 1000,
+  cards: [],
+  currentBet: 0,
+  score: 0
+});
+
+const setInPlayer = (opts, player) => ({
+  chips: opts.chips || player.chips,
+  cards: opts.cards || player.cards,
+  currentBet: opts.currentBet || player.currentBet,
+  score: opts.score || player.score
+});
 
 const initialState = {
-  player: new Player('One', 1000),
-  opponents: [...Array(8)].map(() => new Player(Math.random().toString(36).substr(2, 5), 1000)),
-	mode: 'begin',
+  game: {
+    deck: [],
+    table: {
+      cards: [],
+      chips: 0
+    },
+    stake: 0
+  },
+  player: createPlayer('One'),
+  opponents: [...Array(8)].map(() => createPlayer(Math.random().toString(36).substr(2, 5))), 
+  controlsMode: 'begin'
+};
+
+const begin = state => {
+  const deck = new Deck();
+  deck.shuffle();
+  return Object.assign({}, state, {
+    game: {
+      deck
+    },
+    player: setInPlayer({ cards: deck.deal(2) }, state.player),
+    opponents: state.opponents.map(o => setInPlayer({ cards: deck.deal(2) }, o))
+  });
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case BEGIN:
-      const game = action.payload.game([state.player].concat(state.opponents));
-      game.dealPlayers();
-
-      debugger;
-      return Object.assign({}, state, {
-        player: game.players.find(x => x.name === state.player.name),
-      });
+      return begin(state);
     default:
       return state;
   }
