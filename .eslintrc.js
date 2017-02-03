fe935a60c34078a643a5f5555ff05bb3f3c81add
Dominic Ginger <domginger@hotmail.co.uk>
2017-02-03 23:40:34 +0000

    Split eslint config into seperate file and start using airbnb linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+    },
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "flowtype",
+    ]
+};
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,63 +7,57 @@ import webpackConfig from './webpack.config.babel';
 import mocha from 'gulp-mocha';
 import flow from 'gulp-flowtype';
 
+const configFile = './.eslintrc.js';
 const paths = {
-    allSrcJs: 'src/**/*.js?(x)',
-    serverSrcJs: 'src/server/**/*.js?(x)',
-    sharedSrcJs: 'src/shared/**/*.js?(x)',
-    clientEntryPoint: 'src/client/app.jsx',
-    gulpFile: 'gulpfile.bable.js',
-    webpackFile: 'webpack.config.bable.js',
-    libDir: 'lib',
-    distDir: 'dist',
-    clientBundle: 'dist/client-bundle.js?(.map)',
-    allLibTests: 'lib/test/**/*.js',
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientSrcJs: 'src/client/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.jsx',
+  gulpFile: 'gulpfile.bable.js',
+  webpackFile: 'webpack.config.bable.js',
+  libDir: 'lib',
+  distDir: 'dist',
+  clientBundle: 'dist/client-bundle.js?(.map)',
+  allLibTests: 'lib/test/**/*.js',
 };
 
-gulp.task('clean', () => {
-  return del([
-    paths.libDir,
-    paths.clientBundle,
-  ]);
-});
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
 
-gulp.task('lint', () => {
-    return gulp.src([
-        paths.allSrcJs,
-        paths.gulpFile,
-        paths.webpackFile
-    ])
-    .pipe(eslint({ fix: true }))
-    .pipe(eslint.format())
-    .pipe(flow());
-});
+gulp.task('lint', () => gulp.src([
+    paths.clientSrcJs,
+    paths.gulpFile,
+    paths.webpackFile
+  ])
+  .pipe(eslint({
+    fix: true,
+    configFile
+  }))
+  .pipe(eslint.format())
+  .pipe(flow()));
 
-gulp.task('build', ['lint', 'clean'], () => {
-    return gulp.src(paths.allSrcJs)
-        .pipe(babel())
-        .pipe(gulp.dest(paths.libDir));
-});
+gulp.task('build', ['lint', 'clean'], () => gulp.src(paths.allSrcJs)
+  .pipe(babel())
+  .pipe(gulp.dest(paths.libDir)));
 
 gulp.task('test', ['build'], () =>
   gulp.src(paths.allLibTests)
-    .pipe(mocha())
+  .pipe(mocha()),
 );
 
-gulp.task('dist', () => {
-    return gulp.src(paths.clientEntryPoint)
-        .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest(paths.distDir));
-});
+gulp.task('dist', () => gulp.src(paths.clientEntryPoint)
+  .pipe(webpack(webpackConfig))
+  .pipe(gulp.dest(paths.distDir)));
 
-gulp.task('main', ['test'], () => {
-    return gulp.src(paths.clientEntryPoint)
-        .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest(paths.distDir));
-});
+gulp.task('main', ['test'], () => gulp.src(paths.clientEntryPoint)
+  .pipe(webpack(webpackConfig))
+  .pipe(gulp.dest(paths.distDir)));
 
 gulp.task('watch', () => {
-    gulp.watch(paths.allSrcJs, ['main']);
+  gulp.watch(paths.allSrcJs, ['main']);
 });
 
 gulp.task('default', ['watch', 'main']);
-
